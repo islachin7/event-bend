ff53ccfa066a2ed72b92672bb9b075a9bd1827c6
islachin7 <33438126+islachin7@users.noreply.github.com>
2024-10-15 11:39:43 -0500

    Update App.php cambio de base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/EventosSI/';
+    public string $baseURL = 'http://146.190.211.87:8003/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
