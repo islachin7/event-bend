a9fce2c8a6bbad3b3d2f081ab5a3acdebd860593
islachin7 <islachinvictor7@gmail.com>
2024-10-15 17:00:38 +0000

    delete writable

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'      => '',
         'hostname' => 'localhost',
 		'username' => 'root',
-		'password' => '',
+		'password' => 'dbevento105',
 		'database' => 'bddevento',
 		'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
