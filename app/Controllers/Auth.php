b2d6adc2df846202cacd9c622c0d371cb476a3a9
Victor Islachin <islachinvictor7@gmail.com>
2024-10-16 18:59:58 -0500

    matando cors V2

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -11,6 +11,9 @@ class Auth extends ResourceController
 
   public function __construct(){
 		$this->db = \Config\Database::connect();
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 	}
 
 	public function index()
