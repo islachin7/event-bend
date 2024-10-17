2c233f79cefd8656225fc8e2e232c9381922a186
Victor Islachin <islachinvictor7@gmail.com>
2024-10-16 20:01:47 -0500

    matando cors V6

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -8,28 +8,6 @@ use CodeIgniter\Filters\FilterInterface;
 Class Cors implements FilterInterface
 {
 
-/*
-    public function before(RequestInterface $request, $arguments = null)
-    {
-        $allowedOrigin = 'http://localhost:5173/'; // Cambia esto a tu dominio
-
-        // Establecer encabezados CORS
-        if (isset($_SERVER['HTTP_ORIGIN'])) {
-            if ($_SERVER['HTTP_ORIGIN'] === $allowedOrigin) {
-                header('Access-Control-Allow-Origin: ' . $allowedOrigin);
-            }
-        }
-
-        header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-
-        // Manejar preflight (OPTIONS)
-        if ($request->getMethod() === 'options') {
-            return Services::response()->setStatusCode(204);
-        }
-    }*/
-
-    
     public function before(RequestInterface $request, $arguments = null)
     {
         header("Access-Control-Allow-Origin: *");
