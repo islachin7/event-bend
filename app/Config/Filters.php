459e32ab6c869eccc9750885450cfb749011f28b
Victor Islachin <islachinvictor7@gmail.com>
2024-10-16 19:53:20 -0500

    matando cors V5

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -63,5 +63,11 @@ class Filters extends BaseConfig
      * Example:
      * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
      */
-    public array $filters = [];
+    public array $filters = [
+        // ...
+        'cors' => [
+            'before' => ['api/*'],
+            'after'  => ['api/*'],
+        ],
+    ];
 }
