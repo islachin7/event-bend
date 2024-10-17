862330faf47317a4b8718982cf46b18084470ec4
Victor Islachin <islachinvictor7@gmail.com>
2024-10-16 19:30:02 -0500

    matando cors V4

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -11,9 +11,6 @@ class Auth extends ResourceController
 
   public function __construct(){
 		$this->db = \Config\Database::connect();
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 	}
 
 	public function index()
--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -7,9 +7,32 @@ use CodeIgniter\Filters\FilterInterface;
 
 Class Cors implements FilterInterface
 {
+
+
     public function before(RequestInterface $request, $arguments = null)
     {
-        header('Access-Control-Allow-Origin: *');
+        $allowedOrigin = 'http://localhost:5173/'; // Cambia esto a tu dominio
+
+        // Establecer encabezados CORS
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            if ($_SERVER['HTTP_ORIGIN'] === $allowedOrigin) {
+                header('Access-Control-Allow-Origin: ' . $allowedOrigin);
+            }
+        }
+
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+        // Manejar preflight (OPTIONS)
+        if ($request->getMethod() === 'options') {
+            return Services::response()->setStatusCode(204);
+        }
+    }
+
+    /*
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         $method = $_SERVER['REQUEST_METHOD'];
@@ -17,7 +40,7 @@ Class Cors implements FilterInterface
             die();
         }
     }
- 
+ */
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
       // Do something here
