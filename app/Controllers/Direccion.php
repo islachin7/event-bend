dc63ad4800fec3b5d2299e284e09139e55e697ab
Victor Islachin <islachinvictor7@gmail.com>
2024-10-25 19:24:52 -0500

    Direccion FInal Fix

--- app/Controllers/Direccion.php
+++ app/Controllers/Direccion.php
@@ -6,7 +6,6 @@ use CodeIgniter\API\ResponseTrait;
 class Direccion extends ResourceController
 {
     
-
     public function __construct(){
         $this->db = \Config\Database::connect();
 	}
@@ -60,7 +59,7 @@ class Direccion extends ResourceController
         }
 
         //insert
-        $query = $this->db->query('call insert_direccion("'.$p_descripcion_dire.'")');
+        $query = $this->db->query('call insert_direccion("'.$p_nombre_direccion.'","'.$p_descripcion_dire.'",'.$p_numero_piso.','.$p_aforo_max.')');
         $response = [
             'message'   => 'Se generó con éxito la dirección.'
         ];
@@ -99,13 +98,15 @@ class Direccion extends ResourceController
         
 	}
 
-   // $query = $this->db->query('call update_direccion_acti('.$p_id_direccion.')');
-
     //--------------------------------------------------------------------
 
     public function update_direccion(){
+
         $p_id_direccion =$this->request->getVar('p_id_direccion');
+        $p_nombre_direccion =$this->request->getVar('p_nombre_direccion');
         $p_descripcion_dire =$this->request->getVar('p_descripcion_dire');
+        $p_numero_piso =$this->request->getVar('p_numero_piso');
+        $p_aforo_max =$this->request->getVar('p_aforo_max');
 
         //validacion de id_direccion
         if($p_id_direccion == "" || strlen($p_id_direccion) <= 0){
@@ -127,21 +128,75 @@ class Direccion extends ResourceController
             return $this->respond($response,400);
         }
 
-        //validacion de direccion
+        //validaciones
+        if($p_nombre_direccion == "" || strlen($p_nombre_direccion) <= 0){
+            $response = [
+                'error'     => 'Error, el dirección ingresada esta vacia, llene el campo obligatorio.'
+            ];
+            return $this->respond($response,400);
+        }
+
         if($p_descripcion_dire == "" || strlen($p_descripcion_dire) <= 0){
             $response = [
-            'error'     => 'Error, la dirección ingresada esta vacia, llene el campo obligatorio.'
+                'error'     => 'Error, la descripcion de la dirección ingresada esta vacia, llene el campo obligatorio.'
+            ];
+            return $this->respond($response,400);
+        }
+
+        if($p_numero_piso == "" || strlen($p_numero_piso) <= 0){
+            $response = [
+                'error'     => 'Error, El número de piso ingresado esta vacio, llene el campo obligatorio.'
+            ];
+            return $this->respond($response,400);
+        }
+
+        if($p_aforo_max == "" || strlen($p_aforo_max) <= 0){
+            $response = [
+                'error'     => 'Error, El valor de aforo ingresado esta vacio, llene el campo obligatorio.'
             ];
             return $this->respond($response,400);
         }
 
         //UPDATE
-        $query = $this->db->query('call update_direccion('.$p_id_direccion.',"'.$p_descripcion_dire.'")');
+        $query = $this->db->query('call update_direccion('.$p_id_direccion.',"'.$p_nombre_direccion.'","'.$p_descripcion_dire.'",'.$p_numero_piso.','.$p_aforo_max.')');
         $response = [
             'message'   => 'Se actualizó con éxito la dirección.'
         ];
         return $this->respond($response,200);
 
+	}
+
+    //--------------------------------------------------------------------
+
+    public function activate(){
+        $p_id_direccion =$this->request->getVar('p_id_direccion');
+
+        //validacion de id_direccion
+        if($p_id_direccion == "" || strlen($p_id_direccion) <= 0){
+            $response = [
+            'error'     => 'Error, no existe identificador de dirección.'
+            ];
+            return $this->respond($response,400);
+        }
+
+        //busca direccion
+        $busca = $this->db->query('call listar_direccion('.$p_id_direccion.')');
+        $direccion = $busca->getRowArray();
+
+        //validacion de direccion
+        if($direccion == null){
+            $response = [
+            'error'     => 'Error, no existe la dirección.'
+            ];
+            return $this->respond($response,400);
+        }
+
+        //activar
+        $query = $this->db->query('call update_direccion_acti('.$p_id_direccion.')');
+        $response = [
+            'message'   => 'Se eliminó con éxito la dirección.'
+        ];
+        return $this->respond($response, 200);
 	}
 
     //--------------------------------------------------------------------
@@ -176,8 +231,6 @@ class Direccion extends ResourceController
         ];
         return $this->respond($response, 200);
 	}
-
-    //--------------------------------------------------------------------
 
 
 }
