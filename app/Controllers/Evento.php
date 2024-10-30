fb0c6026ad22a29a91a2b97be70f69918f021afc
Victor Islachin <islachinvictor7@gmail.com>
2024-10-29 19:34:30 -0500

    Fix 5 Evento

--- app/Controllers/Evento.php
+++ app/Controllers/Evento.php
@@ -23,6 +23,21 @@ class Evento extends ResourceController
 
     public function create(){
 
+        $p_nombre_organizador   = "";
+        $p_apellido_organizador = "";
+        $p_nombre_evento        = "";
+        $p_tipo_doc             = "";
+        $p_numero_doc           = "";
+        $p_celular              = "";
+        $p_direccion            = "";
+        $p_correo               = "";
+        $p_fecha_inicio         = "";
+        $p_fecha_fin            = "";
+        $p_categoria_evento     = "";
+        $p_tipo_evento          = "";
+        $p_costo                = "";
+        $p_estado_evento        = "";
+
         $p_nombre_organizador   =$this->request->getVar('p_nombre_organizador');
         $p_apellido_organizador =$this->request->getVar('p_apellido_organizador');
         $p_nombre_evento        =$this->request->getVar('p_nombre_evento');
